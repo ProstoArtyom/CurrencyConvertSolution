a99d7dc35a99b96d3aac4a092dd567c085b27827
ARTYO\artyo <artyom.shashok@mail.ru>
2026-06-27 13:25:55 +0300

    Add currency controller

---
+++ src/CurrencyConvert.WebAPI/Controllers/CurrencyController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CurrencyConvert.WebAPI.Controllers
+{
+
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CurrencyController : ControllerBase
+    {
+        [HttpGet("banks")]
+        public async Task<IActionResult> GetBanks()
+        {
+            return Ok();
+        }
+    }
+}
